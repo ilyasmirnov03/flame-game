561b76988c65b5c8c5857fd939385bd02b65db19
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-02-05 22:29:19 +0100

    feat(user): get groups from ManyToMany relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
+/**
+ * @mixin Builder
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +48,29 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Automatically load relationships.
+     *
+     * @var array
+     */
+    protected $with = [
+        'user_groups'
+    ];
+
+    /**
+     * Get groups of user
+     * @return BelongsToMany
+     */
+    public function user_groups(): BelongsToMany {
+        return $this->belongsToMany(Group::class, 'group_members');
+    }
+
+    /**
+     * Get scores of user
+     * @return HasMany
+     */
+    public function scores(): HasMany {
+        return $this->hasMany(UserScore::class);
+    }
 }
