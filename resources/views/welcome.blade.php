2d7ed0f899f29655e5fb2720d8f431e11a7f5a1d
PMulx <murail.anthonin@gmail.com>
2024-02-06 16:34:43 +0100

    feat(*): setup var css and fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>FlameGame</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <h1>Bienvenu au FlameGame!</h1>
+    <h1>Title coucou H1</h1>
+    <h2>Sous titre H2</h2>
+    <h3>Sous titre H3</h3>
+    <p> Paragraphe assez long</p>
+    <p class="bold"> Paragraphe assez long</p>
+    <button class="btn__green">Avancer</button>
+    <button class="btn__blue">Connexion</button>
 </body>
+
 </html>
