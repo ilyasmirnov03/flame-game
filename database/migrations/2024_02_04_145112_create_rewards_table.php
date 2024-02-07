312090285948cef6fd6e34559f48074d475278c7
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-02-07 16:55:23 +0100

    feat(rewards): selected column on user_rewards

--- database/migrations/2024_02_04_145112_create_rewards_table.php
+++ database/migrations/2024_02_04_145112_create_rewards_table.php
@@ -31,6 +31,7 @@ return new class extends Migration {
                 $table->id();
                 $table->foreignIdFor(User::class);
                 $table->foreignIdFor(Reward::class);
+                $table->boolean('selected')->default(true);
             });
         }
 
