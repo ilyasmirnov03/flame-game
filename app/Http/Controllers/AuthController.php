1b70313372a1e49e042ca6a57af6035dd0704c45
mchauveni <matheo.chaumet.venier@gmail.com>
2024-02-19 18:28:14 +0100

    feat(auth): register,login,logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+
+    public function signup(SignupRequest $request) {
+        $credentials = $request->validated();
+
+        $user = new User();
+        $user->name = $credentials['name'];
+        $user->password = Hash::make($credentials['password']);
+        $user->email = $credentials['email'];
+        $user->save();
+
+        return redirect()->intended(route('home'));
+    }
+
+    /**
+     * Handle an authentication attempt.
+     */
+    public function login(LoginRequest $request): RedirectResponse {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'));
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    /**
+     * Handle a log out
+     */
+    public function logout(): RedirectResponse {
+        Auth::logout();
+        return to_route('profil');
+    }
+}
