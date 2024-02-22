abfe7dab38a88fd84046db46f1fe8044a098a463
mchauveni <matheorigami@gmail.com>
2024-02-22 20:01:01 +0100

    feat(profil): base interface

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -13,9 +13,27 @@
 <body class="daltonism-container">
     @include('nav')
     @include('header')
-    <form action="logout" method="POST">
-        <button>Deconnexion</button>
-    </form>
+    <div class="profilwrapper">
+        <div class="flame">
+            <img class="flame__img" src="{{ asset('images/flamme_logo.svg') }}" alt="Skin de votre flame">
+            <div class="flame__scorewrapper">
+                <h1 class="flame__score"> Score </h1>
+                <h2 class="flame__score"> 3245 </h2>
+            </div>
+        </div>
+        <div class="avatar">
+            <h2 class="avatar__name dyslexie">Mulx</h2>
+            <div class="avatar__displaywrapper">
+                <img class="avatar__display" src="{{ asset('images/avatar.png') }}" alt="votre avatar">
+            </div>
+        </div>
+        <form action="logout" method="POST">
+            @csrf
+            <button class="logout__button">
+                <h2 class="logout__message font dyslexie">Déconnexion</h2>
+            </button>
+        </form>
+    </div>
 </body>
 
 </html>
