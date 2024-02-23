f29aaa58803727bc576c979c13e35f46a25a5c30
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-02-23 21:46:30 +0100

    fix(merge): errors

--- resources/views/games/select_game.blade.php
+++ resources/views/games/select_game.blade.php
@@ -17,7 +17,7 @@
     <section class="games">
         <h1> Choisissez votre défi </h1>
         @foreach(config('static.minigames') as $key => $minigame)
-            <a href="{{ route('game-' . $minigame['route']) }}">
+            <a href="{{ route('flame.game', ['game' => $key]) }}">
                 <img src="{{ asset($minigame['img']) }}" alt="Logo {{ $minigame['label'] }}">
                 <h2>{{ $minigame['label'] }}</h2>
                 <img class="games__info" src="{{ asset('images/info.svg')}}" alt="Info">
