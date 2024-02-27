e5803c1a7a994a613b283cb5825f41df76a6e91d
mchauveni <matheo.chaumet.venier@gmail.com>
2024-02-27 15:36:03 +0100

    feat(profile): edit button that leads to /profile/edit

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -14,6 +14,10 @@
             <div class="avatar__displaywrapper">
                 <img class="avatar__display" src="{{ asset('images/avatar.png') }}" alt="votre avatar">
             </div>
+            @if (Auth::user() == $user)
+                <a class="avatar__edit" href="{{ route('profile.edit') }}">
+                    <img src="{{ asset('images/edit.svg') }}" alt="modifier le profil"></a>
+            @endif
         </div>
         @if (Auth::user() == $user)
             <form action="logout" method="POST">
