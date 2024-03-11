873390e616b61e0ccb84a552069c8078f4c6dd7a
mchauveni <matheo.chaumet.venier@gmail.com>
2024-03-11 15:09:51 +0100

    feat(leaderboard): Podium

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -6,19 +6,41 @@
         <a href="{{ route('leaderboard.group.index') }}">Group</a>
     </div>
     <div class="leaderboard">
-        <div class="leaderboard__podium">
-            <div class="leaderboard__podium--second"></div>
-            <div class="leaderboard__podium--first"></div>
-            <div class="leaderboard__podium--tdird"></div>
-        </div>
+        @if ($page == 1)
+            <div class="podium">
+                <div class="podium__second">
+                    <span>{{ $ranking[1]->name }}</span>
+                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
+                    <div class="podium__image">
+                        <span>{{ $ranking[1]->scores->sum('score') }}</span>
+                    </div>
+                </div>
+                <div class="podium__first">
+                    <span>{{ $ranking[0]->name }}</span>
+                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
+                    <div class="podium__image">
+                        <span>{{ $ranking[0]->scores->sum('score') }}</span>
+                    </div>
+                </div>
+                <div class="podium__third">
+                    <span>{{ $ranking[2]->name }}</span>
+                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
+                    <div class="podium__image">
+                        <span>{{ $ranking[2]->scores->sum('score') }}</span>
+                    </div>
+                </div>
+            </div>
+        @endif
         <table class="leaderboard__ranking">
             <tbody class="ranking">
                 @foreach ($ranking as $user)
-                    <tr class="ranking">
-                        <td class="ranking__rating ranking__cell">#{{ $user->rank }}</td>
-                        <td class="ranking__name ranking__cell">{{ $user->name }}</td>
-                        <td class="ranking__score ranking__cell">{{ $user->scores->sum('score') }}</td>
-                    </tr>
+                    @if ($user->rank > 3)
+                        <tr class="ranking">
+                            <td class="ranking__rating ranking__cell">#{{ $user->rank }}</td>
+                            <td class="ranking__name ranking__cell">{{ $user->name }}</td>
+                            <td class="ranking__score ranking__cell">{{ $user->scores->sum('score') }}</td>
+                        </tr>
+                    @endif
                 @endforeach
             </tbody>
         </table>
