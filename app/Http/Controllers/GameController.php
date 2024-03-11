c4af1bb8c3387f164d0ccdd256bf3769a145994e
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-03-11 11:56:01 +0100

    feat(game): Get different data for game templates

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Factories\GameTemplateData\GameTemplateDataFactory;
 use App\Models\Game;
 use App\Models\Group;
 use Illuminate\View\View;
@@ -10,11 +11,15 @@ class GameController extends Controller {
 
     public function soloGame(Game $game): View
     {
-        return view('games.' . $game->label, ['minigame' => $game]);
+        $data = ['minigame' => $game];
+        GameTemplateDataFactory::getTemplateDataSetter($game->label)?->addToTemplate($data);
+        return view('games.' . $game->label, $data);
     }
 
     public function groupGame(Group $group, Game $game): View
     {
-        return view('games.' . $game->label, ['minigame' => $game, 'group' => $group]);
+        $data = ['minigame' => $game, 'group' => $group];
+        GameTemplateDataFactory::getTemplateDataSetter($game->label)?->addToTemplate($data);
+        return view('games.' . $game->label, $data);
     }
 }
