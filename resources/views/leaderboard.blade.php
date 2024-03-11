9bbff34d48fd16c6624986c81d43d0c42ac5b847
mchauveni <matheo.chaumet.venier@gmail.com>
2024-03-11 16:28:26 +0100

    feat(leaderboard): doesnt display podium if there's no one to get up on it

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -8,39 +8,42 @@
     <div class="leaderboard">
         @if ($page == 1)
             <div class="podium">
-                <div class="podium__second">
-                    <span>{{ $ranking[1]->name }}</span>
-                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
-                    <div class="podium__image">
-                        <span>{{ $ranking[1]->scores->sum('score') }}</span>
+                @if (count($ranking) >= 2)
+                    <div class="podium__second">
+                        <span>{{ $ranking[1]->name }}</span>
+                        <img class="podium__avatar" src="{{ asset($ranking[1]->image) }}" alt="">
+                        <div class="podium__image">
+                            <span>{{ $ranking[1]->scores->sum('score') }}</span>
+                        </div>
                     </div>
-                </div>
-                <div class="podium__first">
-                    <span>{{ $ranking[0]->name }}</span>
-                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
-                    <div class="podium__image">
-                        <span>{{ $ranking[0]->scores->sum('score') }}</span>
+                @endif
+                @if (count($ranking) >= 1)
+                    <div class="podium__first">
+                        <span>{{ $ranking[0]->name }}</span>
+                        <img class="podium__avatar" src="{{ asset($ranking[0]->image) }}" alt="">
+                        <div class="podium__image">
+                            <span>{{ $ranking[0]->scores->sum('score') }}</span>
+                        </div>
                     </div>
-                </div>
-                <div class="podium__third">
-                    <span>{{ $ranking[2]->name }}</span>
-                    <img class="podium__avatar" src="{{ asset('images/avatar.png') }}" alt="">
-                    <div class="podium__image">
-                        <span>{{ $ranking[2]->scores->sum('score') }}</span>
+                @endif
+                @if (count($ranking) >= 3)
+                    <div class="podium__third">
+                        <span>{{ $ranking[2]->name }}</span>
+                        <img class="podium__avatar" src="{{ asset($ranking[2]->image) }}" alt="">
+                        <div class="podium__image">
+                            <span>{{ $ranking[2]->scores->sum('score') }}</span>
+                        </div>
                     </div>
-                </div>
+                @endif
             </div>
         @endif
         <table class="leaderboard__ranking">
             <tbody class="ranking">
                 @foreach ($ranking as $rankable)
                     @if ($rankable->rank > 3)
-                        <tr class="ranking @if ($rankable->id == Auth::id()) ranking--user @endif">
+                        <tr class="ranking">
                             <td class="ranking__rating ranking__cell">#{{ $rankable->rank }}</td>
-                            <td class="ranking__name ranking__cell">{{ $rankable->name }} @if ($rankable->id == Auth::id())
-                                    (vous)
-                                @endif
-                            </td>
+                            <td class="ranking__name ranking__cell">{{ $rankable->name }}</td>
                             <td class="ranking__score ranking__cell">{{ $rankable->scores->sum('score') }}</td>
                         </tr>
                     @endif
