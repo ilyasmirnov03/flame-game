7512ac1986051e39782befe44c3e998975cbe5ce
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-03-12 16:49:50 +0100

    feat(game): Quiz score calculation

--- app/Classes/Factories/Score/QuizScore.php
+++ app/Classes/Factories/Score/QuizScore.php
@@ -2,21 +2,31 @@
 
 namespace App\Classes\Factories\Score;
 
+use App\Models\QuizAnswer;
+
 class QuizScore extends ScoreFactory {
 
     public function calculateScore(string $userId, array $game, int $elapsedTime): array
     {
-        // TODO: Implement calculateScore() method.
+        $maxScore = 1000;
+        $quizAnswer = new QuizAnswer();
+        $rightAnswersAmount = $quizAnswer
+            ->whereIn('id', array_values($game['answers']))
+            ->where('is_right', 1)
+            ->count();
+        $score = ($maxScore / count($game['answers'])) * $rightAnswersAmount;
         return [
-            'score' => 0,
+            'score' => min($score, $maxScore),
             'bonus' => 0,
-            'total' => 0
+            'total' => min($score, $maxScore)
         ];
     }
 
+    /**
+     * Quiz game doesn't have a bonus for anything yet.
+     */
     public function calculateScoreBonus(string $userId, array $game, int $elapsedTime): int
     {
-        // TODO: Implement calculateScoreBonus() method.
         return 0;
     }
 }
