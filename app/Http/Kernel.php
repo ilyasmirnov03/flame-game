0841bf44e88edfc529352f4d4dc8183d2b5f4f28
Ilya Smirnov <ilua.smirnof@gmail.com>
2024-03-19 22:43:59 +0100

    feat(i18n): Set and get locales

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\EnsureUserInGroup;
+use App\Http\Middleware\SetLocale;
 use App\Http\Middleware\UserCanEditOGData;
 use App\Http\Middleware\UserCanPlayGroupGame;
 use App\Http\Middleware\UserCanPlaySoloGame;
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\FunFact::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SetLocale::class,
         ],
 
         'api' => [
